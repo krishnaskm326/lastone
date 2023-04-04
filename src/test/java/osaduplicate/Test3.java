ef18138d8d9861b6779b117f31f70e868028980d
alwin <alwin@Aney>
2023-04-05 04:59:03 +0530

    4th aney

--- src/test/java/osaduplicate/Test3.java
+++ src/test/java/osaduplicate/Test3.java
@@ -1,18 +1,11 @@
 package osaduplicate;
 
 public class Test3 {
-
 	
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 	System.out.println("changed");
 		}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hi this is alwin");
-	}
 
 }
