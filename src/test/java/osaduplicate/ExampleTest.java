0b88d03f3ae0c12aaccb03fb828a272a38570a5c
alwin <alwin@Aney>
2023-04-05 15:18:14 +0530

    extest

--- src/test/java/osaduplicate/ExampleTest.java
+++ src/test/java/osaduplicate/ExampleTest.java
@@ -7,10 +7,7 @@ public class ExampleTest {
 
 	@Test(groups = "smoke")
 	public void data1() {
-		System.out.println("smoke 1 executed");
-	}
-	@Test(groups = "regression")
-	public void data2() {
-		System.out.println("regression 1 executed");
+		System.out.println("smoke 1 aney");
+	
 	}
 }
